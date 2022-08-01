1c2b3493f4a031e0a2204a556f8e74df12df322a
syniepolskyi <101953791+syniepolskyi@users.noreply.github.com>
2022-08-01 16:38:41 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,37 +2,59 @@
 import throttle from 'lodash.throttle';
 
 const form = document.querySelector("form.feedback-form");
-const storageRawData = localStorage.getItem("feedback-form-state");
-const storageData = {
-    email: "",
-    message: ""
+
+
+function readStorage(){
+  const storageRawData = localStorage.getItem("feedback-form-state");
+  const storageData = {
+      email: "",
+      message: ""
+  };
+  if(storageRawData){
+      try{
+          const parsedData = JSON.parse(storageRawData);
+          console.log(`localStorage["feedback-form-state"]`, parsedData);
+          Object.keys(storageData).forEach((key) =>{
+              const elem = document.querySelector(`[name="${key}"]`);
+              storageData[key] = parsedData[key];
+              if(elem){
+                  elem.value = parsedData[key];
+              }
+          });
+      } catch(exc){
+          console.warn(exc);
+      }
+  }
+  return storageData;
 }
 
-if(storageRawData){
-    try{
-        const parsedData = JSON.parse(storageRawData);
-        console.log(`localStorage["feedback-form-state"]`, parsedData);
-        Object.keys(storageData).forEach((key) =>{
-            const elem = document.querySelector(`[name="${key}"]`);
-            storageData[key] = parsedData[key];
-            if(elem){
-                elem.value = parsedData[key];
-            }
-        });
-    } catch(exc){
-        console.warn(exc);
-    }
+function validate(form){
+    const elements = form.elements;
+    const storageData = readStorage();
+    const allFilled = Object.keys(storageData).reduce((acc, key) => {
+      const isEmpty = (elements[key].value.trim() === '');
+      elements[key].classList.toggle('error',isEmpty);
+      return ( acc && (!isEmpty) );
+    }, true);
+    return allFilled;
 }
 
 function onInput(ev){
     //console.log(ev.target.name, ev.target.value);
+    const storageData = readStorage();
     storageData[ev.target.name] = ev.target.value;
-    localStorage.setItem("feedback-form-state", JSON.stringify(storageData));
+    if(validate(ev.target.parentNode.parentNode)){
+      localStorage.setItem("feedback-form-state", JSON.stringify(storageData));
+    }
 }
 
 form.addEventListener("input", throttle(onInput, 500));
 form.addEventListener("submit", (ev) => {
+    const storageData = readStorage();
     ev.preventDefault();
+    if(!validate(ev.currentTarget)){
+      return ;
+    }
     console.log(storageData);
     localStorage.removeItem("feedback-form-state");
     console.log('localStorage["feedback-form-state"] was removed')
